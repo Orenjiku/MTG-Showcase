fa377f65d8276094bc70a411e327576c82685a95
William Chang <wdchang86@gmail.com>
2021-02-10 09:36:26 -0500

    Added new ES6 syntax optional chaining to Card.jsx to handle image cases.

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -12,8 +12,8 @@ const Card = ({ card }) => {
       <img className='cardImage' src={card.image_uris ? card.image_uris.normal : card.card_faces[0].image_uris.normal} onClick={handleOpen}/>
       <Modal className='cardModalContainer' open={open} onClose={handleClose} aria-labelledby={card.name}>
         <div>
-          <img src={card.card_faces ? card.card_faces[0]?.image_uris?.large : card.image_uris.large} onClick={handleClose} />
-          <img src={card.card_faces ? card.card_faces[1]?.image_uris?.large : null} onClick={handleClose} />
+          <img src={card.card_faces?.[0]?.image_uris ? card.card_faces[0].image_uris.large : card.image_uris.large} onClick={handleClose} />
+          <img src={card.card_faces?.[1]?.image_uris ? card.card_faces[1].image_uris.large : null} onClick={handleClose} />
         </div>
       </Modal>
     </div>
